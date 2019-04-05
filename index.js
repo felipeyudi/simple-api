f48741c0a83c4fb2f4f5ba221669008465070fb5
andrelake <andre.lake@hotmail.com>
2019-04-05 00:01:02 -0300

    meu primeiro commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, function() {
-    console.log('Servidor rodando na porta 3000');
+    console.log('Servidor mandolinando na porta 3000');
 });
 
 app.get('/teste', function(request, response) {
@@ -12,5 +12,5 @@ app.get('/teste', function(request, response) {
 
 app.get('/salvar', function(request, response) {
     console.log("acessou a pagina salvar");
-    response.send("<p>Exemplo com XUXA</p><p>exemplo 2</p>");
+    response.send("<p>Exemplo com BENTO</p><p>exemplo 2</p>");
 });
