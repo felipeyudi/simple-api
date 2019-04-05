73b8de20445fc8e74f543c5dfa88461897723570
Felipe Yudi B Ono <felipeyudi@gmail.com>
2019-04-04 23:40:10 -0300

    exemplo de rota Salvar

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ app.get('/teste', function(request, response) {
     console.log('request recebido em Teste');
     response.send("OK.");
 });
+
+app.get('/salvar', function(request, response) {
+    console.log("acessou a pagina salvar");
+    response.send("<p>Exemplo com XUXA</p><p>exemplo 2</p>");
+});
